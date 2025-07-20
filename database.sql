0851167f434128c05fbc4dba537909ae060af502
Preet1307 <preet.mhatre1307@gmail.com>
2025-07-20 11:59:32 +0530

    Add SQL file

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE studentdb;
+USE studentdb;
+CREATE TABLE student_info(
+  name VARCHAR(50),
+  marks INT,
+  grade VARCHAR(1),
+  city VARCHAR(50)
+);
+
+SELECT * FROM student_info;
